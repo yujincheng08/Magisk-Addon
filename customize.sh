ed3a990e9e5fc65c1d82fa52190bacbab63442fb
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2022-12-26 02:28:59 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,6 +2,8 @@ cd "$MODPATH" || abort
 . "$MODPATH"/util_functions.sh
 chmod +x "$MODPATH"/post-fs-data.sh
 set_systemblk_rw
-mkdir -p /system/addon.d/magisk
-cp -af /data/adb/magisk/. /system/addon.d/magisk
+ADDOND=/system/addon.d
+mkdir -p "$ADDOND"/magisk
+cp -af "$MODPATH"/addon.d.sh $ADDOND/99-magisk.sh
+cp -af /data/adb/magisk/. "$ADDOND"/magisk
 exec "$MODPATH"/post-fs-data.sh
