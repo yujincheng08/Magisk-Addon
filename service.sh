bf760b34ecf37539c03009c41dc3af0793834add
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2022-12-26 10:06:54 +0800

    Fix uninstaller

--- service.sh
+++ service.sh
@@ -4,3 +4,4 @@ MODDIR=${0%/*}
 set_systemblk_rw
 mkdir -p /system/addon.d/magisk
 cp -af /data/adb/magisk/. /system/addon.d/magisk
+[ -n "$REMOUNTED" ] && mount -o remount,ro $REMOUNTED || true
--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,16 @@
 SLOT=$(resetprop -n ro.boot.slot_suffix)
 ADDOND=/system/addon.d
 if [ -f $ADDOND ]; then
-    blockdev --setrw "/dev/block/mapper/system$SLOT" 2>/dev/null
-    rm -f ${ADDOND:?}/99-magisk.sh
-    rm -rf ${ADDOND:?}/magisk
+  blockdev --setrw "/dev/block/mapper/system$SLOT" 2>/dev/null
+  REMOUNTED=""
+  if mount | grep ' /system ' | grep -q 'ro'; then
+    mount -o remount,rw /system
+    REMOUNTED="/system"
+  elif mount | grep ' / ' | grep -q 'ro'; then
+    mount -o remount,rw /
+    REMOUNTED="/"
+  fi
+  rm -f ${ADDOND:?}/99-magisk.sh
+  rm -rf ${ADDOND:?}/magisk
+  [ -n "$REMOUNTED" ] && mount -o remount,ro $REMOUNTED || true
 fi
