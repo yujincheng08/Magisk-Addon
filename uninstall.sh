b06e7d7d54fecc48d6cb4d487d4e01867cc6e0f9
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2022-12-26 09:14:43 +0800

    Fix uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,8 @@
 #!/sbin/sh
-export BBBIN=/system/addon.d/magisk/busybox
-export ASH_STANDALONE=1
 SLOT=$(resetprop -n ro.boot.slot_suffix)
 ADDOND=/system/addon.d
-if echo "$3" | $BBBIN grep -q "uninstall"; then
-    if [ -f $ADDOND ]; then
-        blockdev --setrw "/dev/block/mapper/system$SLOT" 2>/dev/null
-        rm -f ${ADDOND:?}/99-magisk.sh
-        rm -rf ${ADDOND:?}/magisk
-    fi
+if [ -f $ADDOND ]; then
+    blockdev --setrw "/dev/block/mapper/system$SLOT" 2>/dev/null
+    rm -f ${ADDOND:?}/99-magisk.sh
+    rm -rf ${ADDOND:?}/magisk
 fi
