977d10453621b596524065a3613837400939c352
Howard20181 <40033067+Howard20181@users.noreply.github.com>
2023-02-07 03:33:26 +0800

    Format with 4 space

--- addon.d.sh
+++ addon.d.sh
@@ -15,108 +15,108 @@ V1_FUNCS=/tmp/backuptool.functions
 V2_FUNCS=/postinstall/tmp/backuptool.functions
 
 if [ -f $V1_FUNCS ]; then
-  . $V1_FUNCS
-  backuptool_ab=false
+    . $V1_FUNCS
+    backuptool_ab=false
 elif [ -f $V2_FUNCS ]; then
-  . $V2_FUNCS
+    . $V2_FUNCS
 else
-  return 1
+    return 1
 fi
 
 initialize() {
-  # Load utility functions
-  . $MAGISKTMP/util_functions.sh
-  readonly NVBASE=/tmp
-  resolve_vars
+    # Load utility functions
+    . $MAGISKTMP/util_functions.sh
+    readonly NVBASE=/tmp
+    resolve_vars
 
-  if $BOOTMODE; then
-    # Override ui_print when booted
-    ui_print() { log -t Magisk -- "$1"; }
-  fi
-  unset OUTFD
-  setup_flashable
+    if $BOOTMODE; then
+        # Override ui_print when booted
+        ui_print() { log -t Magisk -- "$1"; }
+    fi
+    unset OUTFD
+    setup_flashable
 }
 
 main() {
-  if ! $backuptool_ab; then
-    # Wait for post addon.d-v1 processes to finish
-    sleep 5
-  fi
-
-  # Ensure we aren't in /tmp/addon.d anymore (since it's been deleted by addon.d)
-  mkdir -p "$TMPDIR"
-  cd "$TMPDIR" || return 1
-
-  $BOOTMODE || recovery_actions
-
-  if echo "$MAGISK_VER" | grep -q '\.'; then
-    PRETTY_VER=$MAGISK_VER
-  else
-    PRETTY_VER="$MAGISK_VER($MAGISK_VER_CODE)"
-  fi
-  print_title "Magisk $PRETTY_VER addon.d"
-
-  mount_partitions
-  resolve_vars
-  get_flags
-
-  if $backuptool_ab; then
-    # Swap the slot for addon.d-v2
-    if [ ! -z "$SLOT" ]; then
-      case $SLOT in
-      _a) SLOT=_b ;;
-      _b) SLOT=_a ;;
-      esac
+    if ! $backuptool_ab; then
+        # Wait for post addon.d-v1 processes to finish
+        sleep 5
     fi
-  fi
 
-  find_boot_image
+    # Ensure we aren't in /tmp/addon.d anymore (since it's been deleted by addon.d)
+    mkdir -p "$TMPDIR"
+    cd "$TMPDIR" || return 1
 
-  [ -z "$BOOTIMAGE" ] && abort "! Unable to detect target image"
-  ui_print "- Target image: $BOOTIMAGE"
+    $BOOTMODE || recovery_actions
 
-  remove_system_su
-  api_level_arch_detect
-  install_magisk
+    if echo "$MAGISK_VER" | grep -q '\.'; then
+        PRETTY_VER=$MAGISK_VER
+    else
+        PRETTY_VER="$MAGISK_VER($MAGISK_VER_CODE)"
+    fi
+    print_title "Magisk $PRETTY_VER addon.d"
 
-  # Cleanups
-  cd /
-  $BOOTMODE || recovery_cleanup
-  rm -rf "$TMPDIR"
+    mount_partitions
+    resolve_vars
+    get_flags
 
-  ui_print "- Done"
-  exit 0
+    if $backuptool_ab; then
+        # Swap the slot for addon.d-v2
+        if [ ! -z "$SLOT" ]; then
+            case $SLOT in
+            _a) SLOT=_b ;;
+            _b) SLOT=_a ;;
+            esac
+        fi
+    fi
+
+    find_boot_image
+
+    [ -z "$BOOTIMAGE" ] && abort "! Unable to detect target image"
+    ui_print "- Target image: $BOOTIMAGE"
+
+    remove_system_su
+    api_level_arch_detect
+    install_magisk
+
+    # Cleanups
+    cd /
+    $BOOTMODE || recovery_cleanup
+    rm -rf "$TMPDIR"
+
+    ui_print "- Done"
+    exit 0
 }
 
 case "$1" in
 backup)
-  cp -af "$SYSTEM_MAGISKBIN" $MAGISKTMP
-  ;;
+    cp -af "$SYSTEM_MAGISKBIN" $MAGISKTMP
+    ;;
 restore)
-  cp -af $MAGISKTMP "$SYSTEM_MAGISKBIN"
-  ;;
+    cp -af $MAGISKTMP "$SYSTEM_MAGISKBIN"
+    ;;
 pre-backup)
-  # Stub
-  ;;
+    # Stub
+    ;;
 post-backup)
-  # Stub
-  ;;
+    # Stub
+    ;;
 pre-restore)
-  # Stub
-  ;;
+    # Stub
+    ;;
 post-restore)
-  initialize
-  if $backuptool_ab; then
-    su=sh
-    $BOOTMODE && su=su
-    exec $su -c "sh $0 addond-v2"
-  else
-    # Run in background, hack for addon.d-v1
-    (main) &
-  fi
-  ;;
+    initialize
+    if $backuptool_ab; then
+        su=sh
+        $BOOTMODE && su=su
+        exec $su -c "sh $0 addond-v2"
+    else
+        # Run in background, hack for addon.d-v1
+        (main) &
+    fi
+    ;;
 addond-v2)
-  initialize
-  main
-  ;;
+    initialize
+    main
+    ;;
 esac
--- post-fs-data.sh
+++ post-fs-data.sh
@@ -3,11 +3,11 @@
 # addon.d
 . "$MODDIR"/util_functions.sh
 if [ -d /system/addon.d ]; then
-  log -t Magisk "\- Adding addon.d survival script"
-  set_systemblk_rw
-  ADDOND=/system/addon.d
-  cp -af "$MODDIR"/addon.d.sh $ADDOND/99-magisk.sh
-  chmod 755 $ADDOND/99-magisk.sh
-  cp -af $ADDOND/99-magisk.sh $ADDOND/magisk/addon.d.sh
-  [ -n "$REMOUNTED" ] && mount -o remount,ro $REMOUNTED || true
+    log -t Magisk "\- Adding addon.d survival script"
+    set_systemblk_rw
+    ADDOND=/system/addon.d
+    cp -af "$MODDIR"/addon.d.sh $ADDOND/99-magisk.sh
+    chmod 755 $ADDOND/99-magisk.sh
+    cp -af $ADDOND/99-magisk.sh $ADDOND/magisk/addon.d.sh
+    [ -n "$REMOUNTED" ] && mount -o remount,ro $REMOUNTED || true
 fi
