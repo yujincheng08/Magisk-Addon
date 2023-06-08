7f7461a062bcf5bab3b67d5aeff5d761f8926db0
Christian Oder <myself5@carbonrom.org>
2023-06-08 15:05:31 +0200

    post-fs-data: Also set block/by-name/system as RW
    On older A/B devices (e.g. OnePlus 7 Pro on A13) there is no mapper/system_* path. by-name should be available everywhere and works fine.

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,7 +5,8 @@ MODDIR=${0%/*}
 $SLOT=$(resetprop -n ro.boot.slot_suffix)
 if [ -d /system/addon.d ]; then
   log -t Magisk "- Adding addon.d survival script"
-  blockdev --setrw /dev/block/mapper/system$SLOT 2>/dev/null
+  blockdev --setrw /dev/block/mapper/system$SLOT 2>/dev/null || true
+  blockdev --setrw /dev/block/by-name/system$SLOT 2>/dev/null || true
   REMOUNTED=""
   if mount | grep ' /system ' | grep -q 'ro'; then
     mount -o remount,rw /system
